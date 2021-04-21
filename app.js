997617a80f8713ae2a242b65c10817aa4cbc9311
tamal.sen <tamal.sen@hotmail.com>
2021-04-21 22:16:56 +0530

    enabled cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 let db = require('./db/connection')
-
+let cors = require('cors');
 
 // middleware for receiving input from user
 app.use(express.json());
@@ -10,6 +10,13 @@ app.use(express.urlencoded({extended : false}))
 // db connection function 
 db.connect();
 
+// enabling cors
+app.use(cors({
+    optionsSuccessStatus : 200,
+    origin : "*",
+    methods : ['GET', 'PUT', 'POST', 'DELETE'],
+    credentials: true,
+}))
 
 
 // routes folder 
